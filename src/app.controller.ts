d88c61afe8bf883e0b218c6d32b659b0c39a3bd2
Rafael <rafa_soyyo@hotmail.com>
2025-08-15 00:33:02 +0200

    feat() Swagger added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get(['/', '/ping', '/healthcheck'])
+  @ApiOperation({
+    summary: 'Basic endpoint to verify that the API is up and running',
+  })
+  @ApiResponse({ status: 200, description: 'App is running' })
+  @ApiResponse({ status: 500, description: 'App is not reachable' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -9,7 +9,15 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port: number = configService.get<number>('PORT', 3000);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Api')
+    .setDescription('Nest Api')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(port);
   console.info(`API listening on port: ${port}`);
