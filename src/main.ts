436a5cb06e7b6aaa9711d69df4bce122e6d2a314
Rafael <rafa_soyyo@hotmail.com>
2025-08-15 17:15:07 +0200

    feat() Manage exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   const configService = app.get(ConfigService);
   const port: number = configService.get<number>('PORT', 3000);
 
